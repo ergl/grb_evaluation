52643dd3c4b614cd208e83c1d2970b4e4f5a5f3d
ergl <ergl@users.noreply.github.com>
2020-07-17 10:11:46 +0200

    Fast barrier bench

--- scripts/graph/uniform_barrier.r
+++ scripts/graph/uniform_barrier.r
@@ -43,8 +43,18 @@ plot_theme <- theme_minimal(base_size=10) +
           legend.text = element_text(size=6),
           legend.box.background = element_rect(color="black", fill="white"))
 
+legend_title <- ""
+legend_labels <- c("Recompute uniformVC on replicate", "Recompute uniformVC on local broadcast")
+legend_breaks <- c("vary_repl", "vary_repl_fast")
+legend_values <- c("red", "blue")
+
+legend_params <- scale_colour_manual(name=legend_title,
+                                     breaks=legend_breaks,
+                                     labels=legend_labels,
+                                     values=legend_values)
+
 df <- read.csv("../../uniform_replication/barrier/general.csv")
-df_replication <- df[df$exp == "vary_repl", ]
+df_replication <- df[df$exp == "vary_repl_fast", ]
 # df_melted <- melt(df_replication, id='replication')
 # df_replication_latencies <- df_melted[
 #     df_melted$variable %in% c("ronly_median", "wonly_median", "ronly_barrier_median", "wonly_barrier_median")
@@ -52,7 +62,7 @@ df_replication <- df[df$exp == "vary_repl", ]
 # ]
 
 replication_plot <- ggplot(df_replication,
-                           aes(x=factor(replication), y=throughput, group=1, color=1)) +
+                           aes(x=factor(replication), y=throughput, group=exp, color=exp)) +
 
     geom_point(size=1.5) +
     geom_line() +
@@ -61,6 +71,7 @@ replication_plot <- ggplot(df_replication,
                        labels=format_thousand_comma,
                        expand=c(0,0)) +
     coord_cartesian(ylim=c(0,325000)) +
+    legend_params +
     labs(x = "Replication Interval", y = "Throughput (Ktps)") +
     plot_theme
 
@@ -86,10 +97,11 @@ replication_plot <- ggplot(df_replication,
 #     labs(x = "Replication Interval", y = "Mean Readonly Latency (ms)") +
 #     plot_theme
 
-ronly <- ggplot(df_replication, aes(x=factor(replication), y=ronly_median)) +
+ronly <- ggplot(df_replication,
+                aes(x=factor(replication), y=ronly_median, group=exp, color=exp)) +
 
     geom_point(size=1.5) +
-    geom_line(group=1) +
+    geom_line() +
 
     geom_vline(xintercept=2, size=0.5, color="#807F80") +
 
@@ -97,26 +109,40 @@ ronly <- ggplot(df_replication, aes(x=factor(replication), y=ronly_median)) +
                        expand=c(0,0)) +
 
     coord_cartesian(ylim=c(0,1)) +
-
-    labs(x = "Replication Interval", y = "Mean Readonly Latency (ms)") +
+    legend_params +
+    labs(x = "Replication Interval", y = "Median Readonly Latency (ms)") +
     plot_theme
 
-ronly_barrier <- ggplot(df_replication, aes(x=factor(replication), y=ronly_barrier_median)) +
+ronly_barrier <- ggplot(df_replication,
+                        aes(x=factor(replication), y=ronly_barrier_median, group=exp, color=exp)) +
 
     geom_point(size=1.5) +
-    geom_line(group=1) +
+    geom_line() +
 
+    geom_vline(xintercept=2, size=0.5, color="#807F80")
+
+ronly_barrier_log <- ronly_barrier +
+    scale_y_log10() +
+    legend_params +
+    labs(x = "Replication Interval (Post-commit uniform barrier)", y = "Median Readonly Latency (ms)") +
+    plot_theme
+
+ronly_barrier_standalone <- ronly_barrier +
     geom_vline(xintercept=2, size=0.5, color="#807F80") +
 
-    scale_y_log10() +
+    scale_y_continuous(breaks=seq(0, 1, by=0.10),
+                       expand=c(0,0)) +
 
-    labs(x = "Replication Interval (Post-commit uniform barrier)", y = "Mean Readonly Latency (ms)") +
+    coord_cartesian(ylim=c(0,1)) +
+    legend_params +
+    labs(x = "Replication Interval (Post-commit uniform barrier)", y = "Median Readonly Latency (ms)") +
     plot_theme
 
-wonly <- ggplot(df_replication, aes(x=factor(replication), y=wonly_median)) +
+wonly <- ggplot(df_replication,
+                aes(x=factor(replication), y=wonly_median, group=exp, color=exp)) +
 
     geom_point(size=1.5) +
-    geom_line(group=1) +
+    geom_line() +
 
     geom_vline(xintercept=2, size=0.5, color="#807F80") +
 
@@ -124,25 +150,27 @@ wonly <- ggplot(df_replication, aes(x=factor(replication), y=wonly_median)) +
                        expand=c(0,0)) +
 
     coord_cartesian(ylim=c(0,1)) +
-
-    labs(x = "Replication Interval", y = "Mean Update-only Latency (ms)") +
+    legend_params +
+    labs(x = "Replication Interval", y = "Median Update-only Latency (ms)") +
     plot_theme
 
-wonly_barrier <- ggplot(df_replication, aes(x=factor(replication), y=wonly_barrier_median)) +
+wonly_barrier <- ggplot(df_replication,
+                        aes(x=factor(replication), y=wonly_barrier_median, group=exp, color=exp)) +
 
     geom_point(size=1.5) +
-    geom_line(group=1) +
+    geom_line() +
 
     geom_vline(xintercept=2, size=0.5, color="#807F80") +
 
     scale_y_log10() +
-
-    labs(x = "Replication Interval (Post-commit uniform barrier)", y = "Mean Update-only Latency (ms)") +
+    legend_params +
+    labs(x = "Replication Interval (Post-commit uniform barrier)", y = "Median Update-only Latency (ms)") +
     plot_theme
 
 combined <- grid.arrange(replication_plot + theme(legend.position = "none"),
-                         grid.arrange(grid.arrange(ronly, wonly, nrow=1),
-                                      grid.arrange(ronly_barrier, wonly_barrier, nrow=1)), nrow=1)
+                         grid.arrange(grid.arrange(ronly + theme(legend.position = "none"),
+                                                   wonly + theme(legend.position = "none"), nrow=1),
+                                      grid.arrange(ronly_barrier_standalone + theme(legend.position = "none"), wonly_barrier + theme(legend.position = "none"), nrow=1)), nrow=1)
 
 ggsave(filename = "./uniform_barrier_bench.png",
        plot = combined,
@@ -150,3 +178,10 @@ ggsave(filename = "./uniform_barrier_bench.png",
        width = 20,
        height = 10,
        dpi = 300)
+
+# ggsave(filename = "./read_barrier_comparison.png",
+#        plot = ronly_barrier_standalone,
+#        device = "png",
+#        width = 5,
+#        height = 5,
+#        dpi = 300)
