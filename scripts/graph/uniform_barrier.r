a04363f3693cc1bb39a2f60a15be7b4974ae0f03
ergl <ergl@users.noreply.github.com>
2020-08-31 18:09:26 +0200

    Bench

--- scripts/graph/uniform_barrier.r
+++ scripts/graph/uniform_barrier.r
@@ -43,31 +43,25 @@ plot_theme <- theme_minimal(base_size=10) +
           legend.text = element_text(size=6),
           legend.box.background = element_rect(color="black", fill="white"))
 
-wonly_legend_params <- scale_colour_manual(name="",
-                                           labels=c("No Barrier", "Post-commit barrier"),
-                                           breaks=c("wonly_median", "wonly_barrier_median"),
-                                           values=c("red", "blue"))
-
 df <- read.csv("../../uniform_barrier/results.csv")
 df <- df[df$exp == "write_barrier", ]
 
 throughput_plot <- ggplot(df, aes(x=factor(replication), y=throughput,
-                                  group=replication, color=exp)) +
+                                  group=exp, color=exp)) +
     geom_point(size=1.5) +
     geom_line() +
     scale_y_continuous(breaks=seq(0, 1000000, by=5000),
                        labels=format_thousand_comma,
                        expand=c(0,0)) +
-    coord_cartesian(ylim=c(0,10000)) +
+    coord_cartesian(ylim=c(0,100000)) +
     labs(x = "Replication Interval", y = "Throughput (Ktps)") +
     plot_theme
 
 latencies_plot <- ggplot(df, aes(x=factor(replication), y=wonly_barrier_median,
-                                 group=replication, color=exp)) +
+                                 group=exp, color=exp)) +
     geom_point(size=1.5) +
     geom_line() +
-    scale_y_continuous(breaks=seq(0, 150, by=10), expand=c(0,0)) +
-    coord_cartesian(ylim=c(0,150)) +
+    scale_y_log10() +
     labs(x = "Replication Interval", y = "Median Latency (ms)") +
     plot_theme
 
