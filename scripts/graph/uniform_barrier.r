f993ed3df1b08a83ccfbf9a607f98a7726aaa934
ergl <ergl@users.noreply.github.com>
2020-08-31 16:23:03 +0200

    Modify script for new experiment

--- scripts/graph/uniform_barrier.r
+++ scripts/graph/uniform_barrier.r
@@ -43,84 +43,41 @@ plot_theme <- theme_minimal(base_size=10) +
           legend.text = element_text(size=6),
           legend.box.background = element_rect(color="black", fill="white"))
 
-
-ronly_legend_params <- scale_colour_manual(name="",
-                                           labels=c("No Barrier", "Post-commit barrier"),
-                                           breaks=c("ronly_median", "ronly_barrier_median"),
-                                           values=c("red", "blue"))
-
 wonly_legend_params <- scale_colour_manual(name="",
                                            labels=c("No Barrier", "Post-commit barrier"),
                                            breaks=c("wonly_median", "wonly_barrier_median"),
                                            values=c("red", "blue"))
 
-df <- read.csv("../../uniform_replication/barrier/general.csv")
-df_replication <- df[df$exp == "vary_repl_fast", ]
+df <- read.csv("../../uniform_barrier/results.csv")
+df <- df[df$exp == "write_barrier", ]
 
-df_replication_latencies_ronly <- melt(df_replication,
-                                        id.vars = 'replication',
-                                        measure.vars = c("ronly_median",
-                                                         "ronly_barrier_median"))
-
-df_replication_latencies_wonly <- melt(df_replication,
-                                        id.vars = 'replication',
-                                        measure.vars = c("wonly_median",
-                                                         "wonly_barrier_median"))
-
-replication_plot <- ggplot(df_replication,
-                           aes(x=factor(replication), y=throughput, group=exp, color=exp)) +
-
+throughput_plot <- ggplot(df, aes(x=factor(replication), y=throughput,
+                                  group=replication, color=exp)) +
     geom_point(size=1.5) +
     geom_line() +
-    geom_vline(xintercept=2, size=0.5, color="#807F80") +
-    scale_y_continuous(breaks=seq(0, 1000000, by=25000),
+    scale_y_continuous(breaks=seq(0, 1000000, by=5000),
                        labels=format_thousand_comma,
                        expand=c(0,0)) +
-    coord_cartesian(ylim=c(0,325000)) +
+    coord_cartesian(ylim=c(0,10000)) +
     labs(x = "Replication Interval", y = "Throughput (Ktps)") +
     plot_theme
 
-ronly_latencies_plot <- ggplot(df_replication_latencies_ronly,
-                               aes(x=factor(replication), y=value, color=variable, group=variable)) +
+latencies_plot <- ggplot(df, aes(x=factor(replication), y=wonly_barrier_median,
+                                 group=replication, color=exp)) +
     geom_point(size=1.5) +
     geom_line() +
-    geom_vline(xintercept=2, size=0.5, color="#807F80") +
-    scale_y_continuous(breaks=seq(0, 1, by=0.1), expand=c(0,0)) +
-    coord_cartesian(ylim=c(0,0.61)) +
-    ronly_legend_params +
-    labs(title = "Read-only latencies", x = "Replication Interval", y = "Median Latency (ms)") +
+    scale_y_continuous(breaks=seq(0, 150, by=10), expand=c(0,0)) +
+    coord_cartesian(ylim=c(0,150)) +
+    labs(x = "Replication Interval", y = "Median Latency (ms)") +
     plot_theme
 
-wonly_latencies_plot <- ggplot(df_replication_latencies_wonly,
-                               aes(x=factor(replication), y=value, color=variable, group=variable)) +
-    geom_point(size=1.5) +
-    geom_line() +
-    geom_vline(xintercept=2, size=0.5, color="#807F80") +
-    scale_y_log10() +
-    wonly_legend_params +
-    labs(title = "Update latencies", x = "Replication Interval", y = "Median Latency (ms)") +
-    plot_theme
+combined <- grid.arrange(throughput_plot + theme(legend.position = "none"),
+                         latencies_plot + theme(legend.position = "none"),
+                         nrow=1)
 
-get_legend <- function(arg_plot) {
-    tmp <- ggplot_gtable(ggplot_build(arg_plot))
-    leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
-    legend <- tmp$grobs[[leg]]
-    return(legend)
-}
-
-combined_legend <- get_legend(ronly_latencies_plot)
-
-combined <- grid.arrange(replication_plot + theme(legend.position = "none"),
-                         grid.arrange(ronly_latencies_plot + theme(legend.position = "none"),
-                                      wonly_latencies_plot + theme(legend.position = "none"),
-                                      nrow=1),
-                         combined_legend,
-                         ncol=1,
-                         heights=c(0.8,0.8,0.1))
-
-ggsave(filename = "./uniform_barrier_bench.pdf",
+ggsave(filename = "./barrier.pdf",
        plot = combined,
        device = "pdf",
-       width = 9,
-       height = 11,
+       width = 15,
+       height = 8,
        dpi = 300)
