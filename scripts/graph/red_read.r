f116ae256a8d8988e932e66088c585daf2bee16c
ergl <ergl@users.noreply.github.com>
2020-10-07 19:22:30 +0200

    Benchmark

--- scripts/graph/red_read.r
+++ scripts/graph/red_read.r
@@ -42,46 +42,55 @@ plot_theme <- theme_minimal(base_size=10) +
           legend.text = element_text(size=6),
           legend.box.background = element_rect(color="black", fill="white"))
 
-df <- read.csv("../../red_coord_2/results.csv")
-df <- df[df$replicas != 1, ]
-to_keep <- c("cure", "old_uniform", "uniform_blue", "uniform_red")
-df <- df[df$replication %in% to_keep, ]
-df_readonly <- df[df$exp == "reads", ]
-df_updates <- df[df$exp == "updates", ]
+df <- read.csv("../../redblue_evaluation/results.csv")
+df$replication <- factor(df$replication, levels=c('cure', 'uniform',
+                                                  'redblue_blue', 'redblue_red'))
+df$cluster <- factor(df$cluster, levels=c('all', 'virginia',
+                                          'oregon', 'ireland', 'california'))
+df_reads <- df[df$op == "read", ]
+df_reads_overall <- df_reads[df_reads$cluster == "all", ]
+df_reads_each <- df_reads[df_reads$cluster != "all", ]
+df_updates <- df[df$op == "update", ]
 
-reads_title_text <- "Reads only, 1 object, 20ms RTT"
-updates_title_text <- "Updates only, 1 object, 20ms RTT"
+reads_title_text <- "R=1, 20ms RTT"
+updates_title_text <- "W=1, 20ms RTT"
+
+replica_labels <- c(`california` = "Replica C",
+                    `ireland` = "Replica B",
+                    `oregon` = "Replica A",
+                    `virginia` = "Leader")
+
+replica_colors <- c(`california` = "#FE7B4B",
+                    `ireland` = "#4DE481",
+                    `oregon` = "#1C5BD0",
+                    `virginia` = "#F2818F")
 
 scales <- scale_colour_manual(  name=""
                               , labels=c(`cure` = "Cure",
-                                         `old_uniform` = "Uniform",
-                                         `uniform_blue` = "RedBlue (100% blue)",
-                                         `uniform_red` = "RedBlue (100% red)",
-                                         `z_red_commit` = "RedBlue (100% red), commit latency")
+                                         `uniform` = "Uniform",
+                                         `redblue_blue` = "RedBlue (100% blue)",
+                                         `redblue_red` = "RedBlue (100% red)")
 
                               , values=c(`cure` = "red",
-                                         `old_uniform` = "blue",
-                                         `uniform_blue` = "green",
-                                         `uniform_red` = "orange",
-                                         `z_red_commit` = "brown"))
+                                         `uniform` = "blue",
+                                         `redblue_blue` = "green",
+                                         `redblue_red` = "orange"))
 
-throughput_plot <- ggplot(df_readonly, aes(x=factor(replicas), y=throughput,
-                                        group=replication, color=replication)) +
+thr_r_plot <- ggplot(df_reads_overall, aes(x=factor(replicas), y=throughput,
+                                                group=replication, color=replication)) +
 
     geom_point(size=1.5) +
     geom_line() +
     scale_y_continuous(breaks=seq(0, 1000000, by=50000),
                        labels=format_thousand_comma,
                        expand=c(0,0)) +
-
     coord_cartesian(ylim=c(0,800000)) +
     scales +
     labs(title=reads_title_text, x = "Replicas", y = "Max. Throughput (Ktps)") +
     plot_theme
 
-throughput_writes_plot <- ggplot(df_updates, aes(x=factor(replicas), y=throughput,
-                                        group=replication, color=replication)) +
-
+thr_w_plot <- ggplot(df_updates, aes(x=factor(replicas), y=throughput,
+                                     group=replication, color=replication)) +
     geom_point(size=1.5) +
     geom_line() +
     scale_y_continuous(breaks=seq(0, 500000, by=25000),
@@ -93,60 +102,56 @@ throughput_writes_plot <- ggplot(df_updates, aes(x=factor(replicas), y=throughpu
     labs(title=updates_title_text, x = "Replicas", y = "Max. Throughput (Ktps)") +
     plot_theme
 
-latency_plot <- ggplot(df_readonly, aes(x=factor(replicas), group=replication)) +
+lat_r_plot <- ggplot(df_reads_overall, aes(x=factor(replicas), group=replication)) +
 
     geom_point(size=1.5, aes(y=ronly_median, color=replication)) +
     geom_line(aes(y=ronly_median, color=replication)) +
 
-    geom_point(size=1.5, aes(y=red_commit_median, color="z_red_commit")) +
-    geom_line(aes(y=red_commit_median, color="z_red_commit")) +
-
     geom_point(size=1.5, aes(y=red_ronly_median, color=replication)) +
     geom_line(aes(y=red_ronly_median, color=replication)) +
-
-    scale_y_continuous(breaks=seq(0, 50, by=2),
+    scale_y_continuous(breaks=seq(0, 50, by=1),
                        expand=c(0,0)) +
 
-    coord_cartesian(ylim=c(1,40)) +
+    coord_cartesian(ylim=c(0.1,30)) +
     scales +
     labs(x = "Replicas", y = "Median Latency (ms)") +
     plot_theme
 
-latency_writes_plot <- ggplot(df_updates, aes(x=factor(replicas), group=replication, color=replication)) +
+lat_r_each_plot <- ggplot(df_reads_each, aes(x=factor(cluster),
+                                             y=red_ronly_median, fill=cluster)) +
+    geom_bar(stat='identity', position="dodge") +
+    geom_hline(yintercept=20, size=0.5, color="#807F80") +
+    geom_hline(yintercept=30, size=0.5, color="#807F80") +
+    facet_rep_wrap(~replicas, nrow=1, scales="free_x", labeller=labeller(
+        replicas = c(
+            `2` = "Active: 2 Replicas",
+            `3` = "Active: 3 Replicas",
+            `4` = "Active: 4 Replicas"
+        ))
+    ) +
+    scale_x_discrete(labels=replica_labels) +
+    scale_fill_manual(labels=replica_labels,
+                      values=replica_colors) +
+    scale_y_continuous(breaks=seq(0,40,by=2),
+                       expand=c(0,0),
+                       sec.axis = dup_axis(name="")) +
+    coord_cartesian(ylim=c(0,34)) +
+    labs(x = "Replicas", y = "Median Latency (ms)") +
+    plot_theme +
+    theme(legend.position = "none",
+          axis.title.x = element_text(size=9, margin=margin(10,0,0,0)))
 
-    geom_point(size=1.5, aes(y=wonly_median, color=replication)) +
-    geom_line(aes(y=wonly_median, color=replication)) +
-    geom_point(size=1.5, aes(y=red_wonly_median, color=replication)) +
-    geom_line(aes(y=red_wonly_median, color=replication)) +
-    scale_y_continuous(breaks=seq(0, 50, by=2),
+lat_w_plot <- ggplot(df_updates, aes(x=factor(replicas), y=wonly_median,
+                                     group=replication, color=replication)) +
+
+    geom_point(size=1.5) +
+    geom_line() +
+    scale_y_continuous(breaks=seq(0, 3, by=0.25),
                        expand=c(0,0)) +
 
-    coord_cartesian(ylim=c(1,45)) +
+    coord_cartesian(ylim=c(0,3)) +
     scales +
     labs(x = "Replicas", y = "Median Latency (ms)") +
-    plot_theme
-
-df_red_updates <- df_updates[df_updates$replication == "uniform_red", ]
-abort_plot <- ggplot(df_red_updates, aes(x=factor(replicas), y=abort_ratio)) +
-    geom_bar(stat='identity') +
-    scale_y_continuous(breaks=seq(0,1,by=0.05), expand=c(0,0)) +
-    coord_cartesian(ylim=c(0,0.25)) +
-    scales +
-    labs(title="Red Updates only / Abort ratio", x = "Replicas", y = "Abort ratio") +
-    plot_theme
-
-total_red_update_thr_plot <- ggplot(df_red_updates, aes(x=factor(replicas), group=replication))+
-    geom_point(size=1.5, aes(y=throughput, color=replication)) +
-    geom_line(aes(y=throughput, color=replication)) +
-    geom_point(size=1.5, aes(y=total_throughput, color=replication)) +
-    geom_line(aes(y=total_throughput, color=replication)) +
-    scale_y_continuous(breaks=seq(0, 500000, by=25000),
-                       labels=format_thousand_comma,
-                       expand=c(0,0)) +
-
-    coord_cartesian(ylim=c(0,65000)) +
-    scales +
-    labs(title=updates_title_text, x = "Replicas", y = "Max. Throughput (Ktps)") +
     plot_theme
 
 get_legend <- function(arg_plot) {
@@ -156,30 +161,38 @@ get_legend <- function(arg_plot) {
     return(legend)
 }
 
-combined_legend <- get_legend(latency_plot)
+combined_legend <- get_legend(thr_r_plot)
 
-reads <- grid.arrange(throughput_plot + theme(legend.position = "none"),
-                      latency_plot + theme(legend.position = "none"),
+reads <- grid.arrange(thr_r_plot + theme(legend.position = "none"),
+                      lat_r_plot + theme(legend.position = "none"),
                       nrow=1)
 
-updates <- grid.arrange(throughput_writes_plot + theme(legend.position = "none"),
-                        latency_writes_plot + theme(legend.position = "none"),
+updates <- grid.arrange(thr_w_plot + theme(legend.position = "none"),
+                        lat_w_plot + theme(legend.position = "none"),
                         nrow=1)
 
-reads_combined <- grid.arrange(reads, combined_legend,
-                               ncol=1,
-                               heights=c(0.9,0.1))
+# combined <- grid.arrange(reads, updates, combined_legend, ncol=1,
+#                          heights=c(0.50,0.48,0.1))
+
+combined <- grid.arrange(reads, combined_legend, ncol=1,
+                         heights=c(0.9,0.1))
 
 ggsave(filename = "./red_read.pdf",
-       plot = reads_combined,
+       plot = combined,
        device = "pdf",
        width = 10,
-       height = 5,
+       height = 6,
        dpi = 300)
 
-ggsave(filename = "./red_abort.pdf",
-       plot = abort_plot,
-       device = "pdf",
-       width = 5,
-       height = 5,
-       dpi = 300)
+ggsave(filename = "./red_read_each.pdf",
+       plot=lat_r_each_plot,
+       width=8,
+       height=5,
+       dpi=300)
+
+# ggsave(filename = "./red_abort.pdf",
+#        plot = abort_plot,
+#        device = "pdf",
+#        width = 5,
+#        height = 5,
+#        dpi = 300)
